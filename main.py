0a8b7bf40a24f94b83e221cb9c0c38b885b8264d
breckenreed <62158298+breckenreed@users.noreply.github.com>
2023-06-14 17:43:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start', 'hello'])
 def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
+    bot.reply_to(message, "Listen to always awesome news here: http://websdr.ewi.utwente.nl:8901/")
 
 
 @bot.message_handler(func=lambda msg: True)
